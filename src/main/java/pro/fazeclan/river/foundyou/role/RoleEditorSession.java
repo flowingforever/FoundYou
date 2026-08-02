8772ca1e8446bdb2287463ed07c404ec87910bfc
river <refractionisaneatconcept@gmail.com>
2026-08-01 21:26:23 -0400

    wip

---
+++ src/main/java/pro/fazeclan/river/foundyou/role/RoleEditorSession.java
@@ -0,0 +1,11 @@
+package pro.fazeclan.river.foundyou.role;
+
+public class RoleEditorSession {
+
+    private final Role role;
+
+    public RoleEditorSession(Role role) {
+        this.role = role;
+    }
+
+}
